0da7aa2050defe939f2e27201efd0506dfa366ff
LuigiBruno <137228665+LuigiOfic@users.noreply.github.com>
2024-04-11 08:45:32 -0300

    Delete banco directory

--- banco/insert.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO Aluno (num_matricula, nome, turma, email, senha) VALUES
-(12345678, 'João', 'joao@example.com', 'A101', 'senha123'),
-(99924591, 'Maria', 'maria@example.com', 'B202', 'abc123'),
-(01327482, 'Pedro','pedro@example.com', 'C303', 'pass456');
-
-INSERT INTO Professor (nif, nome, email, senha) VALUES
-(80070977, 'Silva', 'silva@example.com', 'ola'),
-(20254394, 'Santos', 'santos@example.com', 'oba'),
-(30356475, 'Oliveira', 'oliveira@example.com', 'bom');
